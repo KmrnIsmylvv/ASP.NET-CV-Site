60ea1cf7ec0cd1077871c7b6bd455982ec94f2b5
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-10-29 13:59:32 +0400

    Add project files.

---
+++ DinamikCVSayti/Default.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DinamikCVSayti
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet1TableAdapters.TBL_HAQQIMDATableAdapter dt = new DataSet1TableAdapters.TBL_HAQQIMDATableAdapter();
+            Repeater1.DataSource = dt.HaqqimdaListele();
+            Repeater1.DataBind();
+        }
+    }
+}
